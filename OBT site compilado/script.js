4d6dc3e5a0e63a89b89c5c3d84d42999569acc22
AntonioGamePlay <133515242+AntonioGamePlay@users.noreply.github.com>
2024-04-26 20:13:08 -0300

    Add files via upload

---
+++ OBT site compilado/script.js
@@ -0,0 +1,1 @@
+const barralateral=document.querySelector(".barralateral"),barrinha=document.querySelector(".barrinha");barrinha.addEventListener("click",()=>{barralateral.classList.toggle("active")});
